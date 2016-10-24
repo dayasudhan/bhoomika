717f27b21135751031a2143e8597ef0588ac87bf
Gagan s patil <Gagan s patil>
2016-10-24 02:12:03 -0700

    all changes done and profile page completed

---
+++ app/src/main/java/khaanavali/customer/NotificationActivity.java
@@ -0,0 +1,7 @@
+package khaanavali.customer;
+
+/**
+ * Created by Gagan on 9/14/2016.
+ */
+public class NotificationActivity {
+}
---
+++ app/src/main/java/khaanavali/customer/adapter/ExpandableListAdapter.java
@@ -0,0 +1,7 @@
+package khaanavali.customer.adapter;
+
+/**
+ * Created by Gagan on 9/14/2016.
+ */
+public class ExpandableListAdapter {
+}
